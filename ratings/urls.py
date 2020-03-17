d70a30b7772222cdd215f2338642858d0139b296
Jack Boreham <sc16jeb@leeds.ac.uk>
2020-03-17 15:33:18 +0000

    Final cleanup

--- ratings/urls.py
+++ ratings/urls.py
@@ -1,12 +1,9 @@
 
 from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
 
-    #path('',views.home, name = 'home'),
-    #path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('listModules/', views.listModules.as_view(), name='modules'),
     path('login/', views.loginUser.as_view(), name='login'),
     path('register/',views.register.as_view(), name='register'),
