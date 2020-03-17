7bdcc817c5f27a4f2ddbf2b5236606892a418558
Jack Boreham <sc16jeb@leeds.ac.uk>
2020-03-17 16:08:39 +0000

    url changes for pythonanywhere 2

--- assignment1/urls.py
+++ assignment1/urls.py
@@ -19,8 +19,9 @@ from ratings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('ratings.urls')),
     path('listModules/', views.listModules.as_view(), name='modules'),
-    path('login/', views.loginUser.as_view(), name='login'),
+    path('login/', views.oginUser.as_view(), name='login'),
     path('register/',views.register.as_view(), name='register'),
     path('view/',views.viewProfessors.as_view(), name='professors'),
     path('specific-module-average/', views.professorModuleScore.as_view() ,name=' professor_module_score'),
